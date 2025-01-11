071c182b659d39845029a57037c6c69f42498ce6
theppasiva <theppasiva@gmail.com>
2025-01-11 15:22:46 +0530

    updated workstation.sh

--- workstation.sh
+++ workstation.sh
@@ -64,3 +64,16 @@ VALIDATE $? "eksctl installation"
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 VALIDATE $? "kubens installation"
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+VALIDATE $? "helm installation"
+
+helm repo add aws-ebs-csi-driver https://kubernetes-sigs.github.io/aws-ebs-csi-driver
+helm repo update
+helm upgrade --install aws-ebs-csi-driver --namespace kube-system aws-ebs-csi-driver/aws-ebs-csi-driver
+VALIDATE $? "aws-ebs-csi-driver installation through helm"
+
+curl -sS https://webinstall.dev/k9s | bash
+VALIDATE $? "k9's installation"
